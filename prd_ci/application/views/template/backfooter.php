85e27cf8a36cfbfee2e49c17de37a7c99f4560fc
assd123ss <111363321+assd123ss@users.noreply.github.com>
2022-08-18 20:22:13 +0700

    a

---
+++ prd_ci/application/views/template/backfooter.php
@@ -0,0 +1,13 @@
+      <!-- Main Footer -->
+      <footer class="main-footer">
+        <!-- To the right -->
+        <div class="pull-right hidden-xs">
+           facebook.com/teccom29 
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; 30128-2006 : Database System
+      </footer>
+    </div><!-- ./wrapper -->
+        </div>
+    </body>
+</html>
