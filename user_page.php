9cd3735b12388b6852a927eb3a392d3e6255c289
hotdy <111363321+assd123ss@users.noreply.github.com>
2022-09-25 20:36:35 +0700

    update

--- user_page.php
+++ user_page.php
@@ -10,7 +10,7 @@ if (!$_SESSION["UserID"]){
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Title</title>
+    <title>หน้าผู้ใช้</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
